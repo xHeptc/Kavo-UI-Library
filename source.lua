04fd5dc44882acfbcf0fdca31a2fc0ed3da22106
xHeptc <67541675+xHeptc@users.noreply.github.com>
2021-08-11 19:05:01 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -2575,6 +2575,7 @@ function Kavo.CreateLib(kavName, themeList)
             	label.Parent = sectionInners
             	label.BackgroundColor3 = themeList.SchemeColor
             	label.BorderSizePixel = 0
+				label.ClipsDescendants = true
             	label.Text = title
            		label.Size = UDim2.new(0, 352, 0, 33)
 	            label.Font = Enum.Font.Gotham
